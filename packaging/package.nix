d718db7b74b89e97ba60401f1fb6cd1dcad24820
Neurofibromin <125222560+Neurofibromin@users.noreply.github.com>
2025-06-07 16:17:56 +0200

    more changes to nix

--- packaging/package.nix
+++ packaging/package.nix
@@ -1,17 +1,16 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import <nixpkgs> { } }:
 
 let
   version = "0.4.2";
   libvlcppSrc = pkgs.fetchFromGitHub {
-      owner = "videolan";
-      repo = "libvlcpp";
-      rev = "master";
-#      sha256 = pkgs.lib.fakeSha256;
-      sha256 = "sha256-nnS4DMz/2VciCrhOBGRb1+kDbxj+ZOnEtQmzs/TJ870=";
-    };
+    owner = "videolan";
+    repo = "libvlcpp";
+    rev = "master";
+    #      sha256 = pkgs.lib.fakeSha256;
+    sha256 = "sha256-nnS4DMz/2VciCrhOBGRb1+kDbxj+ZOnEtQmzs/TJ870=";
+  };
   pname = "mfaomp";
-in
-pkgs.stdenv.mkDerivation {
+in pkgs.stdenv.mkDerivation {
   inherit version;
   inherit pname;
 
@@ -19,11 +18,9 @@ pkgs.stdenv.mkDerivation {
     owner = "Neurofibromin";
     repo = "mfaomp";
     rev = "v${version}";
-#    sha256 = pkgs.lib.fakeSha256;
+    #    sha256 = pkgs.lib.fakeSha256;
     sha256 = "sha256-nh7prQzqPbRQUYzJUZ4cAILdn2ElRdEkiRXyJdrtf9A=";
   };
-
-
 
   nativeBuildInputs = with pkgs; [
     cmake
@@ -47,28 +44,28 @@ pkgs.stdenv.mkDerivation {
   ];
 
   postPatch = ''
-      mkdir -p third_party/libvlcpp
-      cp -r ${libvlcppSrc}/* ${libvlcppSrc}/.??* third_party/libvlcpp/ 2>/dev/null || true
-    '';
+    mkdir -p third_party/libvlcpp
+    cp -r ${libvlcppSrc}/* ${libvlcppSrc}/.??* third_party/libvlcpp/ 2>/dev/null || true
+  '';
   installPhase = ''
     runHook preInstall
     cmake --install . --prefix=$out
     runHook postInstall
   '';
 
-#  already done by cmake:
-#  postInstall = ''
-#
-#     install -Dm644 LICENSE $out/share/doc/${pname}/LICENSE.txt #this doesn't work because pname is not a known ...
-#  '';
+  #  already done by cmake:
+  #  postInstall = ''
+  #
+  #     install -Dm644 LICENSE $out/share/doc/${pname}/LICENSE.txt #this doesn't work because pname is not a known ...
+  #  '';
 
   meta = with pkgs.lib; {
-      description = "Multiple Files At Once Media Player";
-      homepage = "https://github.com/Neurofibromin/mfaomp";
-      license = pkgs.lib.licenses.gpl3Plus;
-      maintainers = with pkgs.lib.maintainers; [ neurofibromin ];
-#      platforms = [ "x86_64-linux" ];
-      platforms = pkgs.lib.platforms.linux;
-      mainProgram = "mfaomp";
-    };
+    description = "Multiple Files At Once Media Player";
+    homepage = "https://github.com/Neurofibromin/mfaomp";
+    license = pkgs.lib.licenses.gpl3Plus;
+    maintainers = with pkgs.lib.maintainers; [ neurofibromin ];
+    #      platforms = [ "x86_64-linux" ];
+    platforms = pkgs.lib.platforms.linux;
+    mainProgram = "mfaomp";
+  };
 }
