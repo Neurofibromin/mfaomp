5387a90e39bef2404156d5dee68b914748d9dc5d
Neurofibromin <125222560+Neurofibromin@users.noreply.github.com>
2025-06-09 11:19:33 +0200

    package.nix use packaged libvclpp

--- packaging/package.nix
+++ packaging/package.nix
@@ -6,17 +6,18 @@
   pkg-config,
   qt6,
   libvlc,
+  libvlcpp,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "mfaomp";
   version = "0.4.2";
-  libvlcppSrc = fetchFromGitHub {
-    owner = "videolan";
-    repo = "libvlcpp";
-    tag = "0.1.0";
-    hash = "sha256-nnS4DMz/2VciCrhOBGRb1+kDbxj+ZOnEtQmzs/TJ870=";
-  };
+#  libvlcppSrc = fetchFromGitHub {
+#    owner = "videolan";
+#    repo = "libvlcpp";
+#    tag = "0.1.0";
+#    hash = "sha256-nnS4DMz/2VciCrhOBGRb1+kDbxj+ZOnEtQmzs/TJ870=";
+#  };
 
   src = fetchFromGitHub {
     owner = "Neurofibromin";
@@ -37,16 +38,17 @@ stdenv.mkDerivation (finalAttrs: {
     qt6.qtwebengine
     qt6.qtsvg
     libvlc
+    libvlcpp
   ];
 
   cmakeFlags = [
-    (lib.cmakeBool "USE_PREDOWNLOADED_LIBVLCPP" true)
+    (lib.cmakeBool "USE_SYSTEM_PROVIDED_LIBVLCPP" true)
   ];
 
-  postPatch = ''
-    mkdir -p third_party/libvlcpp
-    cp -r ${finalAttrs.libvlcppSrc}/* ${finalAttrs.libvlcppSrc}/.??* third_party/libvlcpp/ 2>/dev/null || true
-  '';
+#  postPatch = ''
+#    mkdir -p third_party/libvlcpp
+#    cp -r ${finalAttrs.libvlcppSrc}/* ${finalAttrs.libvlcppSrc}/.??* third_party/libvlcpp/ 2>/dev/null || true
+#  '';
 
   meta = {
     description = "Multiple Files At Once Media Player";
