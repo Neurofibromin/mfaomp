4cc904d472931b1e587fb8c38dd6df4ead24b189
Neurofibromin <125222560+Neurofibromin@users.noreply.github.com>
2025-06-12 15:50:41 +0200

    add c++ functions instead of macros for backend detection

---
+++ BackendAvailability.cpp
@@ -0,0 +1,63 @@
+// BackendAvailability.cpp
+#include "BackendAvailability.h"
+#include <QDebug>
+#include "config.h"
+
+#ifdef HAVE_LIBVLC
+#include <vlc/vlc.h>
+#include "vlc.hpp"
+#endif
+
+#ifdef HAVE_QTMULTIMEDIA
+#include <QtMultimedia>
+#include <QtMultimediaWidgets>
+#include <QMediaPlayer>
+#include <QMediaDevices> // For checking if default audio output exists, etc.
+#endif
+
+#ifdef HAVE_QTWEBENGINE
+#include <QWebEngineView>
+#include <QWebEngineProfile> // Often needed for QWebEngineView initialization
+#endif
+
+namespace mfaomp::BackendAvailability {
+
+bool isVLCAvailable() {
+#ifdef HAVE_LIBVLC
+    libvlc_instance_t* inst = libvlc_new(0, nullptr);
+    if (inst) {
+        libvlc_release(inst);
+        return true;
+    }
+#endif
+    return false;
+}
+
+bool isQtMultimediaAvailable() {
+#ifdef HAVE_QTMULTIMEDIA
+    try {
+        QMediaPlayer player;
+        if (QMediaDevices::audioOutputs().isEmpty()) {
+            // return false; // if no audio outputs means 'unavailable' this could be false, but it can stay true for now
+        }
+        return true;
+    } catch (...) {
+        return false;
+    }
+#endif
+    return false;
+}
+
+bool isQtWebEngineAvailable() {
+#ifdef HAVE_QTWEBENGINE
+    try {
+        QWebEngineView view;
+        return true;
+    } catch (...) {
+        return false;
+    }
+#endif
+    return false;
+}
+}
+
---
+++ BackendAvailability.h
@@ -0,0 +1,10 @@
+#ifndef BACKENDAVAILABILITY_H
+#define BACKENDAVAILABILITY_H
+
+namespace mfaomp::BackendAvailability {
+        bool isVLCAvailable();
+        bool isQtMultimediaAvailable();
+        bool isQtWebEngineAvailable();
+}
+
+#endif //BACKENDAVAILABILITY_H
